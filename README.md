119231f89a3acff30c1e86ab4b8595084b2b7f49
Mike Thorn <mike@TherapyCharts.com>
2015-10-08 09:35:20 -0400

    Improving exception handling, adding documentation

--- README.md
+++ README.md
@@ -1,1 +1,107 @@
 # Router
+A simple PHP router with minimal configuration. It does not use a regular expression, and maps parameters in the url directly to a file name and class name. This code does not care what occurs once these controllers or views are loaded.
+
+Example code:
+
+```php
+$router = DevLucid\Router::get_instance([
+    'parameter_name' => 'todo',  # this is the default value
+    'controller_path' => __DIR__.'controllers/', # this is an example, no default value
+    'view_path' => __DIR__.'views/',       # this is an example, no default value
+]);
+$router->process();
+```
+
+In this code, the call to ->process() will look at $_REQUEST['todo'] for an action to perform. There are two patterns recognized: one for views, and one for controllers. 
+
+### Loading views
+
+Given the above code, the following querystring would load a view named testview.php: ?todo=view/testview 
+
+
+### Loading controllers
+
+Given the above code, the following querystring would include a file named users.php, create an instance of a class named 'users_controller', and call a method named authenticate: ?todo=users/authenticate 
+
+
+## Installation
+The easiest way to install is using composer: 
+
+```bash
+composer require devlucid/router
+```
+
+## Rules for routes
+* if a view's filename starts with an underscore (ex: _my_view.php), then it will not be loaded via ->process(). An exception will be thrown in this case. Such a view may still be loaded from a different view or controller. 
+
+* if the class name for a controller starts with an underscore (ex: _my_test_controller), then it will not be loaded via ->process(). An exception will be thrown in this case. Such a controller may still be loaded from a different view or controller. 
+
+* The path for a view or controller may not move up in the filesystem hierarchy using ../. 
+
+
+## Additional configuration options
+
+### Using URL rewriting via mod_rewrite
+
+In the default configuration, the router looks for a URL parameter named 'todo' to determine which controller or view to load. By using url rewriting, you can 
+
+```apache
+RewriteEngine On
+RewriteBase /
+ 
+# Disable rewrite for valid directory/files     
+RewriteCond %{REQUEST_FILENAME} !-f
+RewriteCond %{REQUEST_FILENAME} !-d 
+RewriteRule ^(.*)$ index.php?todo=$1 [L,QSA,B]
+```
+
+Notably this example assumes that the router entry point is index.php, and that / is the base. 
+
+
+### Using URL rewriting in nginx
+
+I haven't tested this at all. Try something like this:
+
+```nginx
+server {
+    ...
+    rewrite ^(.*)$ index.php?todo=$1 [L,QSA,B]
+    ...
+}
+```
+
+### Changing parameter name
+
+```php
+$router = DevLucid\Router::get_instance([
+    'parameter_name' => 'route',  # this is a custom value
+    'controller_path' => __DIR__.'controllers/', # this is an example, no default value
+    'view_path' => __DIR__.'views/',       # this is an example, no default value
+]);
+$router->process();
+```
+
+Given this configuration, the router will look for $\_REQUEST['route'] instead of $_REQUEST['todo']
+
+### Using a PSR-3 compliant logger:
+
+In this example, assume that $logger contains an instantiated PSR-3-compliant logger. 
+
+```php
+global $logger;
+$router = DevLucid\Router::get_instance([
+    'parameter_name' => 'todo',  # this is the default value
+    'controller_path' => __DIR__.'controllers/', # this is an example, no default value
+    'view_path' => __DIR__.'views/',       # this is an example, no default value
+    'logger' => $logger,
+]);
+$router->process();
+```
+
+Whenever a view is loaded, you should see a message like this in your debug log:
+
+```log
+Loading view /var/www/dev/app/views/_nav1_logged_in.php
+```
+
+Whenever a controller is loaded, you should see a message like this in your debug log:
--- Router.php
+++ Router.php
@@ -16,6 +16,23 @@ class Router
         $this->logger          = null;
     }
 
+    private function throw_exception($msg)
+    {
+        if (is_object($this->logger))
+        {
+            $this->logger->error($msg);
+        }
+        throw new \Exception($msg);
+    }
+
+    private function log_debug($msg)
+    {
+        if (is_object($this->logger))
+        {
+            $this->logger->debug($msg);
+        }
+    }
+
     public static function get_instance($config=[])
     {
         if (null !== static::$instance) {
@@ -24,63 +41,95 @@ class Router
         static::$instance = new Router();
         foreach ($config as $key=>$value)
         {
-            static::$instance->$key = $value;
+            if ($key === 'view_path' or $key === 'controller_path')
+            {
+                static::$instance->$key = realpath($value);
+            }
+            else
+            {
+                static::$instance->$key = $value;
+            }
+            
         }
 
         return static::$instance;
     }
 
-    public static function controller($name)
+    public static function check_path_sanity($base_path, $file_path)
+    {
+        if(strpos($file_path, $base_path) !== 0 || strpos($file_path, $base_path) === false)
+        { 
+            Router::get_instance()->throw_exception('Invalid path found. A view or controller path must not move up in the filesystem hierarchy. Tried to load '.$file_path.' from base path '.$base_path); 
+        }
+    }
+
+    public static function controller($name,$allow_underscore_initial = true)
     {
         $router = Router::get_instance();
-        $controller_path  = $router->controller_path.$name.'.php';
-        $controller_class = $name .'_controller';
-
-        if (is_object($router->logger))
+        
+        $controller_class ='DevLucid\\'.(str_replace('/','_',$name)) .'_controller';
+        if ($allow_underscore_initial === false)
         {
-            $this->logger->debug('Loading controller class '.$controller_class.' from file '.$controller_path);
-        }
-
-        if (!file_exists($controller_path))
-        {
-            if (is_object($router->logger))
+            if (strpos($controller_class,'_') === 0)
             {
-                $router->logger->error('Could not locate controller file. Expected to find '.$controller_path);
+                $router->throw_exception('A controller cannot be called via url if its class name starts with an underscore. It may still be called from another controller method or view.');
             }
-            throw new \Exception('Could not locate controller file. Expected to find '.$controller_path);
         }
-        include($controller_path);
 
+        # only bother loading the controller class if it doesn't exist
         if (!class_exists($controller_class))
         {
-            if (is_object($router->logger))
+            $controller_path  = realpath($router->controller_path .'/'. $name . '.php');
+            if (is_null($controller_path) or $controller_path === '')
             {
-                $router->logger->error('Could not locate controller class in file '.$controller_path.'. Expected to find '.$controller_class);
+                $router->throw_exception('Invalid path found. Could not map controller '.$name.' to actual file');
             }
-            throw new \Exception('Could not locate controller class in file '.$controller_path.'. Expected to find '.$controller_class);
+
+            Router::check_path_sanity($router->controller_path, $controller_path);
+            
+            $router->log_debug('Loading controller class '.$controller_class.' from file '.$controller_path);
+
+            if (!file_exists($controller_path))
+            {
+                $router->throw_exception('Could not locate controller file. Expected to find '.$controller_path);
+            }
+            include($controller_path);
+
+            if (!class_exists($controller_class))
+            {
+                $router->throw_exception('Could not locate controller class in file '.$controller_path.'. Expected to find '.$controller_class);
+            }
         }
         $controller = new $controller_class();
 
         return $controller;
     }
 
-    public static function view($name)
+    public static function view($name,$allow_underscore_initial = true)
     {
         $router = Router::get_instance();
-        $view_path = $router->view_path.$name.'.php';
 
-        if (is_object($router->logger))
+        $view_path = realpath($router->view_path . '/'.$name . '.php');
+        if (is_null($view_path) or $view_path === '')
         {
-            $router->logger->debug('Loading view '.$view_path);
+            $router->throw_exception('Invalid path found. Could not map view '.$name.' to actual file');
         }
+        Router::check_path_sanity($router->view_path, $view_path);
+        
+        if ($allow_underscore_initial === false)
+        {
+            $file_name = basename($view_path);
+            if (strpos($file_name,'_') === 0)
+            {
+                $router->throw_exception('A view file cannot be called via url if its name starts with an underscore. It may still be called from another view that does not start with an underscore, or from a controller method.');
+            }
+        }
+
+        $router->log_debug('Loading view '.$view_path);
 
         if (!file_exists($view_path))
         {
-            if (is_object($router->logger))
-            {
-                $router->logger->error('Could not locate view file. Expected to find '.$view_path);
-            }
-            throw new \Exception('Could not locate view file. Expected to find '.$view_path);
+            $router->throw_exception('Could not locate view file. Expected to find '.$view_path);
         }
 
         return include($view_path);
@@ -95,20 +144,20 @@ class Router
         }
         $parts = explode('/',$_REQUEST[$this->parameter_name]);
         
-        if (count($parts) !== 2)
-        {
-
-            throw new \Exception('Invalid format for router parameter. The format should be [controller]/[Method name], or view/[view name]. ');
-        }
-
         if ($parts[0] == 'view')
         {
-            \DevLucid\Router::view($parts[1]);
+            array_shift($parts);
+            \DevLucid\Router::view(implode('/',$parts),false);
         }
         else
         {
-            $controller = \DevLucid\Router::controller($parts[0]);
-            $method_name = $parts[1];
+            $method_name = array_pop($parts);
+            if (strpos($method_name,'_') === 0)
+            {
+                $router->throw_exception('A controller method cannot be called via url if its name starts with an underscore. It may still be called from another controller method or view that does not start with an underscore.');
+            }
+            $controller = \DevLucid\Router::controller(implode('/',$parts),false);
+            
             $controller->$method_name();
         }
     }
